ed218ff9ada122d2d57faa8660275924f184eda1
Kelvin Kiprop <kelvin.kiprop96@outlook.com>
2019-07-01 22:52:29 +0300

    changed works gallery layout added new images

--- res/js/index.js
+++ res/js/index.js
@@ -1,6 +1,9 @@
 function init() {
     var elems = document.querySelectorAll('.sidenav');
     var instance = M.Sidenav.getInstance(elems);
+
+    var materialBox = document.querySelectorAll('.materialboxed');
+    var instance2 = M.Materialbox.init(materialBox);
 }
 
 init();
